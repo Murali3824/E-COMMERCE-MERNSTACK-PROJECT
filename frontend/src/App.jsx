2a3d6376de6a367a4160e676c51d6978202ef3c3
murali <muralinetha38@gmail.com>
2024-09-29 19:11:02 +0530

    second commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home'
+import Collection from './pages/Collection';
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/placeorder' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
