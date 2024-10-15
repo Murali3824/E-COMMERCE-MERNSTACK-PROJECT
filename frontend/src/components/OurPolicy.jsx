f00348cbea14bea5fef46af07b1991747b1c519f
murali <muralinetha38@gmail.com>
2024-10-15 06:25:45 +0530

    frontend

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -3,19 +3,19 @@ import { assets } from '../assets/assets';
 
 const OurPolicy = () => {
     return (
-        <div className='mt-16 sm:px-[5vw] md:px-[6vw] lg:px-[7vw]  flex flex-row  gap-3.5 sm:gap-12 text-center  text-xs sm:text-sm md:text-base text-gray-700'>
+        <div className='mt-16 sm:px-[5vw] md:px-[6vw] lg:px-[7vw]  flex flex-row justify-center  gap-3.5 sm:gap-12 text-center  text-xs sm:text-sm md:text-base text-gray-700'>
             <div>
-                <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
+                <img src={assets.exchange_icon} className='w-10 sm:w-10 md:w-12 m-auto mb-5' alt="" />
                 <p className='font-semibold'>Easy Exchange Policy</p>
                 <p className='text-gray-400'>We Offer free exchange Policy</p>
             </div>
             <div>
-                <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="" />
+                <img src={assets.quality_icon} className='w-8 sm:w-10 md:w-12 m-auto mb-5' alt="" />
                 <p className='font-semibold'>7 Days Return Policy</p>
                 <p className='text-gray-400'>We provide 7 days free return policy</p>
             </div>
             <div>
-                <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
+                <img src={assets.support_img} className='w-8 sm:w-10 md:w-12 m-auto mb-5' alt="" />
                 <p className='font-semibold'>Best Customer</p>
                 <p className='text-gray-400'>We provide 24/7 customer support_img</p>
             </div>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: true, // or '0.0.0.0' to bind to all interfaces
+    host: true, 
+    port:5173
   },
   plugins: [react()],
-  server:{
-    port:5173
-  }
 })
