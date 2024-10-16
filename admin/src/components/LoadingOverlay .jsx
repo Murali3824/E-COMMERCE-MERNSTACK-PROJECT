dd23780d0acfcdc45837545c2d418b904213fb6c
murali <muralinetha38@gmail.com>
2024-10-17 01:28:12 +0530

    admin

--- admin/src/components/LoadingOverlay .jsx
+++ admin/src/components/LoadingOverlay .jsx
@@ -4,7 +4,7 @@ import ReactLoading from 'react-loading';
 const LoadingOverlay = ({ type, color }) => {
     return (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="text-2xl text-white font-medium">Loading...</div>
+            <div className="text-2xl text-white font-medium">Loading..</div>
             <ReactLoading type={type} color={color}/>
         </div>
     );
